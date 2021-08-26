0156fcf8b5a38da0478fd69ee6a3b23fb7706423
harry <harryyin98@gmail.com>
2021-08-26 14:59:08 +0800

    with PUT POST request

--- blueprints/documented_endpoints/entities/__init__.py
+++ blueprints/documented_endpoints/entities/__init__.py
@@ -1,23 +1,26 @@
 from flask import request, jsonify
-from flask_restplus import Namespace, Resource, fields
+from flask_restplus import Namespace, Resource, fields, marshal
 from http import HTTPStatus
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import json
 cred = credentials.Certificate({
-    
-  })
+     })
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 namespace = Namespace('entities', 'Entities fake endpoints')
 
 entity_model = namespace.model('Entity', {
     'id': fields.String(
-        readonly=True,
+        required=True,
         description='Entity identifier'
     ),
     'food': fields.String(
+        required=True,
+        description='Entity name'
+    ),
+    'price': fields.String(
         required=True,
         description='Entity name'
     )
@@ -29,9 +32,7 @@ entity_list_model = namespace.model('EntityList', {
         description='List of entities',
         as_list=True
     ),
-    'total_records': fields.Integer(
-        description='Total number of entities',
-    ),
+    
 })
 
 entity_example = {'id': 1, 'name': 'Entity name'}
@@ -41,39 +42,55 @@ class entities(Resource):
     '''Get entities list and create new entities'''
 
     @namespace.response(500, 'Internal Server error')
-    @namespace.marshal_list_with(entity_list_model)
+    #@namespace.marshal_list_with(entity_list_model)
     def get(self):
         '''List with all the entities'''
-        snapshot = db.collection('orders').document('9313a565-3839-2595-8175-c072eadd92d7').get()
-        entity_list = snapshot.to_dict()
+        entity = {}
+        entity_list = []
+        z = {}
+        docs = db.collection('orders').stream()
+        for doc in docs:
+            Entity = doc.to_dict()
+            #entity_list.append("Order_ID")
+            entity_list.append(doc.id)
+            entity_list.append(Entity)
+
         return {
-            'entities': entity_list,
-            
+            'Order information': entity_list
         }
 
+@namespace.route('/<string:menuID>')
+class entities(Resource):
     @namespace.response(400, 'Entity with the given name already exists')
     @namespace.response(500, 'Internal Server error')
-    @namespace.expect(entity_model)
-    @namespace.marshal_with(entity_model, code=HTTPStatus.CREATED)
-    def post(self):
-        '''Create a new entity'''
+    @namespace.expect(entity_list_model, validate=True)
+    @namespace.marshal_with(entity_list_model)#, code=HTTPStatus.CREATED)
+    def post(self,menuID):
+        '''Create a new menu'''
+        menu_ref = db.collection(u'menus').document(menuID)
+        newlist=namespace.payload
+        menu_ref.set(
+                  newlist                    
+                )
+        
+        return newlist
+        
 
-        if request.json['name'] == 'Entity name':
-            namespace.abort(400, 'Entity with the given name already exists')
-
-        return entity_example, 201
-
-@namespace.route('/<int:entity_id>')
+@namespace.route('/<string:entity_id>')
 class entity(Resource):
     '''Read, update and delete a specific entity'''
 
     @namespace.response(404, 'Entity not found')
     @namespace.response(500, 'Internal Server error')
-    @namespace.marshal_with(entity_model)
+    #@namespace.marshal_with(entity_model)
     def get(self, entity_id):
         '''Get entity_example information'''
-
-        return entity_example
+        snapshot = db.collection('orders').document(entity_id).get()
+        entity_list = snapshot.to_dict()
+        return {
+            'Order information': entity_list
+            
+        }
 
     @namespace.response(400, 'Entity with the given name already exists')
     @namespace.response(404, 'Entity not found')
@@ -82,11 +99,15 @@ class entity(Resource):
     @namespace.marshal_with(entity_model)
     def put(self, entity_id):
         '''Update entity information'''
-
-        if request.json['name'] == 'Entity name':
+        menu_ref = db.collection(u'menus').document(entity_id)
+        menu_ref.update({
+                 entity_model.data
+                                        
+                    })
+        if request.json['id'] == 'Entity id':
             namespace.abort(400, 'Entity with the given name already exists')
 
-        return entity_example
+        return entity_model.data
 
     @namespace.response(204, 'Request Success (No Content)')
     @namespace.response(404, 'Entity not found')
